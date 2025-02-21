512a6630719e9a9b26fb307a1fe3dcc0186e8e02
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-02-21 16:25:54 -0500

    lint

--- login/login.js
+++ login/login.js
@@ -1,61 +1,61 @@
 // Handle form submission
 document.addEventListener("DOMContentLoaded", async () => {
-    // WARN: DO NOT ADD ANY CODE ABOVE submitHandler() CALL.
-    // explanation: upon submitting form, the redirection might add email and password publicly, in queries,
-    // so we must wait for submitHandler to initiate before we add ANY code.
-    submitHandler()
+  // WARN: DO NOT ADD ANY CODE ABOVE submitHandler() CALL.
+  // explanation: upon submitting form, the redirection might add email and password publicly, in queries,
+  // so we must wait for submitHandler to initiate before we add ANY code.
+  submitHandler()
 
-    const loginHref = document.getElementById("signupHref")
-    loginHref.addEventListener("click", async (e) => {
-        e.preventDefault()
+  const loginHref = document.getElementById("signupHref")
+  loginHref.addEventListener("click", async (e) => {
+    e.preventDefault()
 
-        navigateWithQueries("/signup")
-    })
-    let next = getNext()
-    if (!next) {
-        // TODO: need to build dashboard
-        next = "/dashboard"
-    }
+    navigateWithQueries("/signup")
+  })
+  let next = getNext()
+  if (!next) {
+    // TODO: need to build dashboard
+    next = "/dashboard"
+  }
 
-    if ((await apiCallGet(`${BASE_URL}/auth/authenticated`)).ok) {
-        navigate(next)
-    }
+  if ((await apiCallGet(`${BASE_URL}/auth/authenticated`)).ok) {
+    navigate(next)
+  }
 })
 
 function submitHandler() {
-    let next = getNext()
-    if (!next) {
-        // TODO: need to build dashboard
-        next = "/dashboard"
+  let next = getNext()
+  if (!next) {
+    // TODO: need to build dashboard
+    next = "/dashboard"
+  }
+
+  const loginForm = document.getElementById("loginForm")
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    try {
+      const response = await apiCallPost(
+        `${BASE_URL}/auth/signin`,
+        JSON.stringify({
+          username,
+          password,
+        }),
+      )
+
+      const data = await response.json()
+      if (!response.ok) {
+        alert("Invalid credentials")
+      } else {
+        setToken(data["token"])
+        navigate(next)
+      }
+    } catch (error) {
+      alert("Something went wrong, please try again. If the problem persists, please contact support.")
+      console.error("Login error:", error)
     }
-
-    const loginForm = document.getElementById("loginForm")
-
-    loginForm.addEventListener("submit", async (e) => {
-        e.preventDefault()
-
-        const username = document.getElementById("username").value
-        const password = document.getElementById("password").value
-
-        try {
-            const response = await apiCallPost(
-                `${BASE_URL}/auth/signin`,
-                JSON.stringify({
-                    username,
-                    password,
-                }),
-            )
-
-            const data = await response.json()
-            if (!response.ok) {
-                alert("Invalid credentials")
-            } else {
-                setToken(data["token"])
-                navigate(next)
-            }
-        } catch (error) {
-            alert("Something went wrong, please try again. If the problem persists, please contact support.")
-            console.error("Login error:", error)
-        }
-    })
+  })
 }
--- signup/signup.js
+++ signup/signup.js
@@ -1,62 +1,62 @@
 // Handle form submission
 document.addEventListener("DOMContentLoaded", () => {
-    if (!document.cookie.includes("token")) {
-        alert("You must be logged in to access this page.")
-        navigateWithQueries("/login")
+  if (!document.cookie.includes("token")) {
+    alert("You must be logged in to access this page.")
+    navigateWithQueries("/login")
+  }
+
+  const signupForm = document.getElementById("signupForm")
+  const email = document.getElementById("email")
+  const username = document.getElementById("username")
+  const password = document.getElementById("password")
+  const confirmPassword = document.getElementById("confirmPassword")
+
+  password.value = confirmPassword.value = "temp"
+  let emailManuallyEdited = false
+  email.addEventListener("input", () => {
+    emailManuallyEdited = true
+  })
+
+  // Add listener to username input to update email
+  username.addEventListener("input", (e) => {
+    if (!emailManuallyEdited) {
+      const username = e.target.value
+      email.value = username ? `${username}@psu.edu` : ""
+    }
+  })
+
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    // Basic validation
+    if (password.value !== "temp" && password.value !== confirmPassword) {
+      alert("Passwords do not match!")
+      return
     }
 
-    const signupForm = document.getElementById("signupForm")
-    const email = document.getElementById("email")
-    const username = document.getElementById("username")
-    const password = document.getElementById("password")
-    const confirmPassword = document.getElementById("confirmPassword")
+    try {
+      const response = await apiCallPost(
+        `${BASE_URL}/auth/signup`,
+        JSON.stringify({
+          username: username.value,
+          email: email.value,
+          password: password.value,
+        }),
+      )
+      if (!response.ok) {
+        alert("Something went wrong. Please try again.")
+      } else {
+        alert("Account created successfully!")
+      }
+    } catch (error) {
+      console.error("Signup error:", error)
+      // Handle error appropriately
+    }
+  })
 
-    password.value = confirmPassword.value = "temp"
-    let emailManuallyEdited = false
-    email.addEventListener("input", () => {
-        emailManuallyEdited = true
-    })
-
-    // Add listener to username input to update email
-    username.addEventListener("input", (e) => {
-        if (!emailManuallyEdited) {
-            const username = e.target.value
-            email.value = username ? `${username}@psu.edu` : ""
-        }
-    })
-
-    signupForm.addEventListener("submit", async (e) => {
-        e.preventDefault()
-
-        // Basic validation
-        if (password.value !== "temp" && password.value !== confirmPassword) {
-            alert("Passwords do not match!")
-            return
-        }
-
-        try {
-            const response = await apiCallPost(
-                `${BASE_URL}/auth/signup`,
-                JSON.stringify({
-                    username: username.value,
-                    email: email.value,
-                    password: password.value,
-                }),
-            )
-            if (!response.ok) {
-                alert("Something went wrong. Please try again.")
-            } else {
-                alert("Account created successfully!")
-            }
-        } catch (error) {
-            console.error("Signup error:", error)
-            // Handle error appropriately
-        }
-    })
-
-    const loginHref = document.getElementById("loginHref")
-    loginHref.addEventListener("click", async (e) => {
-        e.preventDefault()
-        navigateWithQueries("/login")
-    })
+  const loginHref = document.getElementById("loginHref")
+  loginHref.addEventListener("click", async (e) => {
+    e.preventDefault()
+    navigateWithQueries("/login")
+  })
 })
